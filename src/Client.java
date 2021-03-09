ca88904a91f0a63dd1da71e7c22b7b2cde4bdeb4
Josh Willox <39120423+jcwillox@users.noreply.github.com>
2021-03-09 14:30:48 +1100

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,80 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+public class Client {
+    final String HOST = "127.0.0.1";
+    final int PORT = 50000;
+
+    private final Socket socket;
+    private final InputStream input;
+    private final OutputStream output;
+
+    public Client() throws IOException {
+        socket = new Socket(HOST, PORT);
+        input = socket.getInputStream();
+        output = socket.getOutputStream();
+    }
+
+    /** Reads and returns available data from the server */
+    public String read() {
+        int c;
+        String line = "";
+
+        // read while available
+        try {
+            do {
+                c = input.read();
+                line += (char) c;
+            } while (input.available() > 0);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error while reading from socket!");
+            System.exit(1);
+        }
+
+        System.out.println("RECV: " + line);
+
+        if (line.equals("ERR")) {
+            System.out.println("Received error from server, aborting!");
+            this.close();
+            System.exit(1);
+        }
+
+        return line;
+    }
+
+    /** Send a message to the server */
+    public void send(String message) {
+        try {
+            output.write(message.getBytes(StandardCharsets.UTF_8));
+            output.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        System.out.println("SENT: " + message);
+    }
+
+    /** Quit and close connection to the server */
+    public void quit() {
+        send("QUIT");
+        read();  // RECV: QUIT
+        close();
+    }
+
+    /** Close connection to the server */
+    public void close() {
+        try {
+            input.close();
+            output.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Error while closing socket connection!");
+            System.exit(1);
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // setup initial connection to the server
+        Client client = new Client();
+        client.send("HELO");
+        client.read();
+        client.send("AUTH admin");
+        client.read();
+        client.send("REDY");
+        client.read();
+
+        // handle job scheduling
+        // ...
+
+        // quit and close connection
+        client.quit();
+    }
+}
