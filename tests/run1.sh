6282c138c961ba31a246be76923f61afb189b10c
Josh Willox <39120423+jcwillox@users.noreply.github.com>
2021-03-31 16:53:39 +1100

    Add testing files

---
+++ tests/run1.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+shopt -s globstar
+
+# ensure we are in the same directory as the script
+DIR=$(dirname "$0")
+cd "$DIR" || exit
+
+echo "compiling java files"
+javac "$(realpath ../src/main)"/**/*.java -d . -verbose
+
+# prevent test script from killing this process
+trap -- '' SIGTERM
+
+echo "running tests"
+bash ./tests1.sh main/Main.class
+
+rm -rf ./main
+echo "removed generated class files"
+
+echo "difference: '$(cat ./S1testConfigs/stage1.diff)'"
