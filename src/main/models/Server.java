6d85b7df321a2891b4281269edbffebf021878c1
kelkon <kelvinkwong0222@gmail.com>
2021-04-01 13:51:29 +1100

    Update Server class constructor to be consistent with Job class (#3)

--- src/main/models/Server.java
+++ src/main/models/Server.java
@@ -6,7 +6,7 @@ import main.Constants;
 public class Server {
     public final String type;
     public final int id;
-    public final String state;  // inactive, booting, idle, active, unavailable
+    public final String state; // inactive, booting, idle, active, unavailable
     public final int startTime;
     public final int core;
     public final int memory;
@@ -16,20 +16,35 @@ public class Server {
 
     private final Client client;
 
-    public Server(Client client, String line) {
-        String[] info = line.split(" ");
-        this.type = info[0];
-        this.id = Integer.parseInt(info[1]);
-        this.state = info[2];
-        this.startTime = Integer.parseInt(info[3]);
-        this.core = Integer.parseInt(info[4]);
-        this.memory = Integer.parseInt(info[5]);
-        this.disk = Integer.parseInt(info[6]);
-        this.waitingJobs = Integer.parseInt(info[7]);
-        this.runningJobs = Integer.parseInt(info[8]);
+    public Server(Client client, String type, int id, String state, int startTime, int core, int memory, int disk,
+            int waitingJobs, int runningJobs) {
+        this.type = type;
+        this.id = id;
+        this.state = state;
+        this.startTime = startTime;
+        this.core = core;
+        this.memory = memory;
+        this.disk = disk;
+        this.waitingJobs = waitingJobs;
+        this.runningJobs = runningJobs;
         this.client = client;
 
         System.out.println(Constants.YELLOW + this.toString());
+    }
+
+    public static Server fromGetServers(Client client, String line) {
+        String[] info = line.split(" ");
+        String type = info[0];
+        int id = Integer.parseInt(info[1]);
+        String state = info[2];
+        int startTime = Integer.parseInt(info[3]);
+        int core = Integer.parseInt(info[4]);
+        int memory = Integer.parseInt(info[5]);
+        int disk = Integer.parseInt(info[6]);
+        int waitingJobs = Integer.parseInt(info[7]);
+        int runningJobs = Integer.parseInt(info[8]);
+
+        return new Server(client, type, id, state, startTime, core, memory, disk, waitingJobs, runningJobs);
     }
 
     public Job[] getJobs() {
@@ -42,6 +57,7 @@ public class Server {
 
     @Override
     public String toString() {
-        return String.format("<Server type='%s' id=%d state='%s' core=%d memory=%d, disk=%d>", type, id, state, core, memory, disk);
+        return String.format("<Server type='%s' id=%d state='%s' core=%d memory=%d, disk=%d>", type, id, state, core,
+                memory, disk);
     }
 }
