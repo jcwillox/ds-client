aebea50eb7df6b42e8222c76b8fe65bd3b0d4237
shreybs <shreyas.llama@gmail.com>
2021-04-05 21:34:48 +1000

    Updated Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,6 +1,9 @@
 package main;
 
 import java.io.IOException;
+import java.util.Arrays;
+import main.models.Job;
+import main.models.Server;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,10 +14,21 @@ public class Main {
         client.send("AUTH admin");
         client.read();
         client.send("REDY");
-        client.read();
+        String read = client.read();
+
+        Server[] serversArray = client.getAllServers(); // array sort to get the largest server
+        Arrays.sort(serversArray, (t1, t2) -> {
+            if (t1.core > t2.core)
+                return -1;
+            if (t1.memory > t2.memory)
+                return -1;
+            else
+                return Integer.compare(t2.disk, t1.disk);
+        });
 
         // handle job scheduling
-        // ...
+        Job job = Job.fromScheduleJob(client, read.split(" ", 2)[1]);
+        job.schedule(serversArray[0]);
 
         // quit and close connection
         client.quit();
